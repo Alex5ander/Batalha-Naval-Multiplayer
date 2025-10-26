61dbd27455aba376f3f4925b80fa140813c1fda3
Alex5ander <alexsandergutierreez@gmail.com>
2025-10-25 21:04:45 -0300

    added crazygames SDK

--- public/js/assets.js
+++ public/js/assets.js
@@ -7,7 +7,7 @@ Crosshair.src = '../images/crosshair.png';
 const loadAssets = async (callback) => {
   await Promise.all([MarkerTile, Crosshair].map(e => new Promise(resolve => e.onload = resolve)));
   try {
-    // await window.CrazyGames.SDK.init();
+    await window.CrazyGames.SDK.init();
     await window.CrazyGames.SDK.game.loadingStart();
     await window.CrazyGames.SDK.game.loadingStop();
   } catch (e) {
